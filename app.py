3da76b387b20e5d79e198b696ec3ae402d14890f
rat soni <andryll.silva@outlook.com>
2023-12-10 21:53:18 -0300

    csv file

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
+import csv
+import pyperclip
 import pyautogui
-from time import sleep
+import time
+
+def cadastrar_produto(nome_produto, valor, descricao):
+    pyautogui.click(718,277, duration= 1)
+    pyautogui.click(144,347, duration= 1)
+    
+    # Etapa 1: Nome do Produto
+    pyperclip.copy(nome_produto)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.press('tab')
+
+    # Etapa 2: Valor
+    pyperclip.copy(valor)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.click(357,262, duration=1)
+    pyautogui.click(1573,453, duration=1)
+
+    # Etapa 3: Descrição
+    pyperclip.copy(descricao)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.press('enter')
+
+def cadastrar_produtos_arquivo(arquivo):
+    with open(arquivo, newline='') as csvfile:
+        leitor_csv = csv.DictReader(csvfile)
+        for linha in leitor_csv:
+            nome = linha['nome'].encode('latin-1').decode('utf-8')
+
+            valor = linha['preco'].encode('latin-1').decode('utf-8')
+
+            descricao = linha['descricao'].encode('latin-1').decode('utf-8')
 
 
-with open('cadastro.txt','r') as cadastro:
-    for line in cadastro:
-        product = line.split(',')[0]
-        price = line.split(',')[1]
-        description = line.split(',')[2]
-        
-        pyautogui.click(716, 280, duration=2)  #cadastrar produto
-        
-        pyautogui.click(160, 340, duration=1)
-        pyautogui.write(product)
-        
-        pyautogui.click(500, 350, duration=1)
-        pyautogui.write(price)
-        
-        pyautogui.click(337, 264, duration=1)
-        pyautogui.click(1580, 445, duration=1)
-        pyautogui.write(description)
-        
-        pyautogui.click(1865, 995, duration=1)
+            cadastrar_produto(nome, valor, descricao)
+            time.sleep(1)  # Espera um segundo entre cada cadastro para evitar problemas de velocidade
 
+# Nome do arquivo CSV
+arquivo_csv = 'products.csv'
 
+# Chama a função para cadastrar os produtos do arquivo CSV
+cadastrar_produtos_arquivo(arquivo_csv)
